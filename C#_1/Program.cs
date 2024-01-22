caadc36d2172c6ba5849f00a58c19b875f60e5e7
k3ssL <viqwer3@yandex.ru>
2024-01-22 14:56:34 +0500

    C#1

---
+++ C#_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int number;
+        for (int i = 0; i == 0;)
+        {
+            Console.Write("Введите число. Число 0 приведет к завершению программы:");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number == 0)
+            {
+                Console.WriteLine("Вы ввели 0, программа завершена");
+                break;
+            }
+            if (number % 2 == 0)
+            {
+                int odd = number;
+                int sum = number + odd;
+                Console.WriteLine($"Сумма четных чисел {sum}");
+            }
+            else if (number % 2 != 0)
+            {
+                int even = number;
+                int raz = number - even;
+                Console.WriteLine($"Разность нечетных чисел {raz}");
+            }
+        }
+    }
+}
