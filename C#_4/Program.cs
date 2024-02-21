9f2e7f5b3ac4b4d36fd896baae0041c604283621
k3ssL <124335116+k3ssL@users.noreply.github.com>
2024-02-21 09:27:24 +0500

    Add files via upload

---
+++ C#_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число с которого пойдет таблица: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            void tableMultiplication(int num)
+            {
+                for (int i = 0; i < 11; i++) 
+                {
+                    int tableResult = num * i;
+                    Console.WriteLine($"{num} * {i} = {tableResult}");
+                }
+            }
+            tableMultiplication(number);
+            Console.ReadLine();
+        }
+    }
+}
